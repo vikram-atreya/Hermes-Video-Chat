e9a561d7e2c6e230f52c6ebd4730ed6364d9663a
vikram-atreya <vikram.atreya@gmail.com>
2021-06-13 09:36:09 +0530

    blink calling and bg change

--- client/src/components/Notifications.jsx
+++ client/src/components/Notifications.jsx
@@ -1,4 +1,5 @@
 import React, { useContext } from 'react';
+import Blink from 'react-blink-text';
 import { Button } from '@material-ui/core';
 
 import { SocketContext } from '../Context';
@@ -10,7 +11,7 @@ const Notifications = () => {
     <>
       {call.isReceivingCall && !callAccepted && (
         <div style={{ display: 'flex', justifyContent: 'space-around' }}>
-          <h1>{call.name} is calling:</h1>
+          <h1><Blink color="white" text={call.name} fontSize="7">Call blinking text</Blink>is calling</h1>
           <Button variant="contained" color="primary" onClick={answerCall}>
             Answer
           </Button>
